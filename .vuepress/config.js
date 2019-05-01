54f45aa70773a20a372b8ab70ff60775e31bada8
ShuangYa <896640547@qq.com>
2019-05-01 12:29:05 +0800

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  base: "/",
+  title: "地图工坊",
+  description: "地图工坊",
+  markdown: {
+    lineNumbers: true,
+    toc: {
+      includeLevel: [1, 2, 3]
+    }
+  },
+  plugins: ['vuepress-plugin-baidu-tongji', {
+    hm: '44b41bc63385a6b27e692272bb1fb393'
+  }],
+  themeConfig: {
+    repo: "sylingd/ow-workshop",
+    editLinks: true,
+    lang: 'zh-CN',
+    editLinkText: '在 GitHub 上编辑此页',
+    nav: [{
+      text: '首页',
+      link: '/'
+    }],
+    sidebar: [
+      '/'
+    ]
+  }
+};
