008626f2a2f17cd1a5bee2849ef311b2f546e699
ShuangYa <896640547@qq.com>
2019-06-24 12:02:12 +0800

    更新结构

--- .vuepress/list.js
+++ .vuepress/list.js
@@ -6,38 +6,30 @@ module.exports = [{
 		]
 	},
 	{
-		title: '功能手册',
+		title: '教程',
+		children: [
+			'/tutorial/zero_based',
+			'/tutorial/mutation',
+			'/tutorial/2d',
+			'/tutorial/aiming_and_perspective',
+			'/tutorial/observer',
+			'/tutorial/core'
+		]
+	},
+	{
+		title: '手册',
 		children: [{
 				title: '参考手册',
 				path: 'https://www.yuque.com/zhaohua-zgi3w/overwatch-document'
 			},
-			'/manual/old',
 			'/manual/array',
 			'/manual/continued_event',
-			'/manual/observer',
-			'/manual/vector',
-			'/manual/core',
-			'/manual/tips'
+			'/manual/vector'
 		]
 	},
 	{
-		title: '教程示例',
-		children: [
-			'/demo/',
-			'/demo/zero_based',
-			'/demo/mutation',
-			'/demo/aiming_and_perspective',
-			'/demo/2d'
-		]
+		title: '国服作品',
+		path: 'https://www.owmod.net/'
 	},
-	{
-		title: '作品收集',
-		children: [{
-				title: '国服作品',
-				path: 'https://www.owmod.net/'
-			},
-			'/works/foreign',
-			'/works/submit',
-		]
-	}
+	'/foreign'
 ];
